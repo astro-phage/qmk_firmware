c7c6cae74c0149fcdd1bbb4fe121b8772b1c5ebe
Astro-phage <ahgj7tow@proxlo.com>
2026-03-01 18:26:09 +0100

    Set default color to solid orange

--- keyboards/keychron/q5_max/ansi_encoder/keymaps/via/config.h
+++ keyboards/keychron/q5_max/ansi_encoder/keymaps/via/config.h
@@ -2,3 +2,11 @@
 
 #undef DYNAMIC_KEYMAP_LAYER_COUNT
 #define DYNAMIC_KEYMAP_LAYER_COUNT 5
+
+
+#define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_SOLID_COLOR
+#define RGB_MATRIX_DEFAULT_HUE 11       // dark orange (~25° mapped to 0-255)
+#define RGB_MATRIX_DEFAULT_SAT 255      // full saturation
+#define RGB_MATRIX_DEFAULT_VAL 255      // max brightness
+#define RGB_MATRIX_DEFAULT_SPD 127
+#define RGB_MATRIX_DEFAULT_ON true
